2fe7961e547557b2e9dd0bf4dc4841b5b5a6157f
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-02-04 15:49:17 +0800

    Update ci_build_android.bash

--- ci_build_android.bash
+++ ci_build_android.bash
@@ -15,10 +15,8 @@ fi
 
 export TARGET=$NDK_TARGET-linux-android
 export TOOLCHAIN=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64
-export CFLAGS="-flto=thin -Wno-int-conversion -fwhole-program-vtables"
+export CFLAGS="-O3 -Wno-array-bounds -flto=thin -Wno-int-conversion -fwhole-program-vtables -Wno-ignored-attributes -Wno-array-bounds -Wno-unknown-warning-option -Wno-ignored-attributes -flto=thin -Wno-int-conversion -fwhole-program-vtables -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=0 -D__GCC_HAVE_SYNC_COMPARE_AND_SWAP_4=1"
 export CXXFLAGS="-D__GCC_HAVE_SYNC_COMPARE_AND_SWAP_4=1"
-export CONFIGURE_FLAGS="--with-malloc-conf=percpu_arena:percpu,background_thread:true" 
-export EXTRA_CFLAGS="-O3 -Wno-array-bounds -flto=thin -Wno-int-conversion -fwhole-program-vtables -Wno-ignored-attributes -Wno-array-bounds -Wno-unknown-warning-option -Wno-ignored-attributes"
 export ANDROID_INCLUDE=$TOOLCHAIN/sysroot/usr/include
 export CPPFLAGS="-I$ANDROID_INCLUDE -I$ANDROID_INCLUDE/$TARGET -mllvm -polly"
 export PATH=$TOOLCHAIN/bin:$PATH
@@ -55,9 +53,16 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+./autogen.sh
 ./configure \
   --enable-autogen "$@" \
   --host=$TARGET \
+  --disable-initial-exec-tls \
+  --with-jemalloc-prefix='je_' \
+  --disable-stats \
+  --disable-fill \
+  --with-lg-page=16 \
+  --enable-doc=no \
   --prefix=${PWD}/build_android-$BUILD_ARCH \
   || error_code=$?
 
